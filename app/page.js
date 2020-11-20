b15d8af08bcc430e60f611e0311c132f71cf0b1e
Dmitry <Dima@Weaver-PC.Hedgehog-MSI>
2020-11-20 17:49:20 +0300

    Изменена логика подключения скрипта к страницам
    - Удалена библиотека JQuery из репазитория
    - Теперь скрипт подключемый к страницам не являеться внешним скриптом

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 'use strict';
 
-(function () {
-  var headElement = $('head');
-
-  headElement.append('<link rel="stylesheet" media="all" href="https://dmitry-407.github.io/Betatest-ivi-Redmine/style.css">');
-  headElement.append('<script src="https://dmitry-407.github.io/Betatest-ivi-Redmine/script.js"></script>');
+(function () {  
+  var script = document.createElement ("script");
+  
+  script.src = chrome.extension.getURL("script.js");
+  script.async = false;
+  
+  document.documentElement.appendChild (script);
 })();
--- app/script.js
+++ app/script.js
@@ -1,8 +1,15 @@
 'use strict';
 
 (function () {
+  const head = $('head');
   const ENTER_KEY = 'Enter';
     
+  /*
+  * Подгрузка стилей
+  */
+  
+  head.append('<link rel="stylesheet" media="all" href="https://dmitry-407.github.io/Betatest-ivi-Redmine/style.css">');
+  
   /*
   * Общие функции
   */
@@ -40,7 +47,7 @@
     
     if (!container.classList.contains('nbl-input_state_disabled')) {
       container.classList.add('nbl-input_state_focused');
-      container.classList.remove('error');
+      container.classList.remove('nbl-input_state_error');
     }
   };
 
@@ -91,7 +98,14 @@
   
   var nextBlockForm = function () {
     if (activeBlockFormLoginLeval == 1) {
-      const email = $('.messages ~ form #username').val();
+      const emailElement = $('.messages ~ form #username');
+      const email = emailElement.val();
+      
+      if (email.length == 0) {
+        emailElement.addClass('nbl-input_state_error');
+        
+        return;
+      }
       
       activeBlockFormLoginLeval++;
       
@@ -201,9 +215,7 @@
   
   if (members.length) {
     var membersLabels = $('.members p span.label');
-    
-    console.log(membersLabels);
-    
+        
     for (var i = 0; i < membersLabels.length; i++) {
       var memberLabel = $(membersLabels[i]);
       var textMemverLavel = memberLabel.text().replace(/.\s*$/, "");
@@ -223,4 +235,16 @@
   }
   
   $('div#activity dt.me').parent().addClass('me');
+  
+  /*
+  * Замена текста в кнопках на иконки
+  */
+  
+  const myAccountElement = topMenuElement.find('.my-account');
+  
+  myAccountElement.html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" id="user_outline_28"><g fill="none" fill-rule="evenodd"><path d="M0 0h28v28H0z"></path><path d="M17.5 8.5C17.5 6.566 15.934 5 14 5a3.499 3.499 0 00-3.5 3.5c0 1.934 1.566 3.5 3.5 3.5s3.5-1.566 3.5-3.5zm2 0c0 3.039-2.461 5.5-5.5 5.5a5.499 5.499 0 01-5.5-5.5C8.5 5.461 10.961 3 14 3s5.5 2.461 5.5 5.5zM7 20.643c0 .943-.08.857.456.857h13.588c.536 0 .456.086.456-.857 0-2.288-3.304-3.643-7.25-3.643S7 18.355 7 20.643zm-2 0C5 16.763 9.299 15 14.25 15s9.25 1.763 9.25 5.643c0 2.016-.781 2.857-2.456 2.857H7.456C5.78 23.5 5 22.66 5 20.643z" fill="currentColor" fill-rule="nonzero"></path></g></svg>');
+  
+  const logoutElement = topMenuElement.find('.logout');
+  
+  logoutElement.html('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 28 28" id="door_arrow_right_outline_28"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.357 3H13a1 1 0 110 2h-2.6c-1.137 0-1.929 0-2.546.051-.605.05-.953.142-1.216.276a3 3 0 00-1.311 1.311c-.134.263-.226.611-.276 1.216C5.001 8.471 5 9.264 5 10.4v7.2c0 1.137 0 1.929.051 2.546.05.605.142.953.276 1.216a3 3 0 001.311 1.311c.263.134.611.226 1.216.276.617.05 1.41.051 2.546.051H13a1 1 0 110 2h-2.643c-1.084 0-1.958 0-2.666-.058-.728-.06-1.369-.185-1.96-.487a5 5 0 01-2.186-2.185c-.302-.592-.428-1.232-.487-1.961C3 19.6 3 18.727 3 17.643v-7.286c0-1.084 0-1.958.058-2.666.06-.728.185-1.369.487-1.96A5 5 0 015.73 3.544c.592-.302 1.233-.428 1.961-.487C8.4 3 9.273 3 10.357 3zm8.936 6.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L21.586 15H12a1 1 0 110-2h9.586l-2.293-2.293a1 1 0 010-1.414z" fill="currentColor"></path></svg>');
 })();
